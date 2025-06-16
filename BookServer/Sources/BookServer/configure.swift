58e55bf14f15c7abd44dcc3356d522801fdf0b08
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-16 15:50:25 +0200

    App configure

--- BookServer/Sources/BookServer/configure.swift
+++ BookServer/Sources/BookServer/configure.swift
@@ -8,9 +8,19 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
-    app.migrations.add(CreateTodo())
+    app.migrations.add(CreateBook())
+    
+    try await app.autoMigrate()
+    
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .DELETE, .OPTIONS],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(corsMiddleware, at: .beginning)
 
     // register routes
     try routes(app)
