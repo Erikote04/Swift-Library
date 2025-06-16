b244e991f95e728e54dddcd8d597a787e94f9a04
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-16 15:15:16 +0200

    Created Book migration

---
+++ BookServer/Sources/BookServer/Migrations/CreateBook.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  BookServer
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/6/25.
+//
+
+import Fluent
+
+struct CreateBook: AsyncMigration {
+    
+    func prepare(on database: Database) async throws {
+        try await database.schema("books")
+            .id()
+            .field("title", .string, .required)
+            .field("author", .string, .required)
+            .field("isbn", .string, .required)
+            .field("publication_year", .int, .required)
+            .field("created_at", .datetime)
+            .field("updated_at", .datetime)
+            .unique(on: "isbn")
+            .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        try await database.schema("books").delete()
+    }
+}
