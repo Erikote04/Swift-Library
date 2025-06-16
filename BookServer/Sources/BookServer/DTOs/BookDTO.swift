832be5e8666860c7fdefcc359e6a9520fa9566d7
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-16 15:42:41 +0200

    DTO to Model

--- BookServer/Sources/BookServer/DTOs/BookDTO.swift
+++ BookServer/Sources/BookServer/DTOs/BookDTO.swift
@@ -9,25 +9,43 @@ import Fluent
 import Vapor
 
 struct BookDTO: Content {
-    let id: UUID
-    let title: String
-    let author: String
-    let isbn: String
-    let publicationYear: Int
+    let id: UUID?
+    let title: String?
+    let author: String?
+    let isbn: String?
+    let publicationYear: Int?
     let createdAt: Date?
     let updatedAt: Date?
     
-    init(from book: Book) throws {
-        guard let id = book.id else {
-            throw Abort(.internalServerError, reason: "Book ID is nil")
+    func toModel() -> Book {
+        let model = Book()
+        
+        model.id = self.id
+        
+        if let title = self.title {
+            model.title = title
         }
         
-        self.id = id
-        self.title = book.title
-        self.author = book.author
-        self.isbn = book.isbn
-        self.publicationYear = book.publicationYear
-        self.createdAt = book.createdAt
-        self.updatedAt = book.updatedAt
+        if let author = self.author {
+            model.author = author
+        }
+        
+        if let isbn = self.isbn {
+            model.isbn = isbn
+        }
+        
+        if let publicationYear = self.publicationYear {
+            model.publicationYear = publicationYear
+        }
+        
+        if let createdAt = self.createdAt {
+            model.createdAt = createdAt
+        }
+        
+        if let updatedAt = self.updatedAt {
+            model.updatedAt = updatedAt
+        }
+        
+        return model
     }
 }
