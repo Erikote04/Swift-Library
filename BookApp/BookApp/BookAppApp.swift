994ae27499e85f6071b521842ea0db96402272b3
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-16 17:27:24 +0200

    Init app project

---
+++ BookApp/BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
