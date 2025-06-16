b01131bdb0094613e3064e36c42b70019f47cbb5
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-16 17:36:53 +0200

    DateFormatter utils

---
+++ BookApp/BookApp/Utils/DateFormatter+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter+Extension.swift
+//  BookApp
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/6/25.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let iso8601Full: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
