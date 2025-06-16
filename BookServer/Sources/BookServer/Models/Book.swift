f00912c13e72a5ad3ce85457fc6169469bed18ce
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-16 15:12:09 +0200

    Created Book model

---
+++ BookServer/Sources/BookServer/Models/Book.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  BookServer
+//
+//  Created by Erik Sebastian de Erice Jerez on 16/6/25.
+//
+
+import Foundation
+import Fluent
+
+final class Book: Model, @unchecked Sendable {
+    static let schema = "books"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "author")
+    var author: String
+    
+    @Field(key: "isbn")
+    var isbn: String
+    
+    @Field(key: "publication_year")
+    var publicationYear: Int
+    
+    @Timestamp(key: "created_at", on: .create)
+    var createdAt: Date?
+    
+    @Timestamp(key: "updated_at", on: .update)
+    var updatedAt: Date?
+    
+    init() { }
+    
+    init(id: UUID? = nil, title: String, author: String, isbn: String, publicationYear: Int) {
+        self.id = id
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.publicationYear = publicationYear
+    }
+}
