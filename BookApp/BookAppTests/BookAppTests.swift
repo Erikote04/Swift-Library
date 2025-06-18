8bc359adfacfcf245694623024a4e1849480f8ad
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2025-06-18 10:53:56 +0200

    Added testing target to client target

---
+++ BookApp/BookAppTests/BookAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookAppTests.swift
+//  BookAppTests
+//
+//  Created by Erik Sebastian de Erice Jerez on 18/6/25.
+//
+
+import Testing
+
+struct BookAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
